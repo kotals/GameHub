4d08e59f7054e2bc71ddac8fcf0eddee0d3685da
Alexey <serebryakov.alx@gmail.com>
2021-06-13 22:09:05 +0300

    greeting

--- client/src/components/GameFromMenu/GameFromMenu.jsx
+++ client/src/components/GameFromMenu/GameFromMenu.jsx
@@ -7,28 +7,28 @@ const GameFromMenu = () => {
   const [game, setGame] = useState({})
   const [trailer, setTrailer] = useState({})
 
-  useEffect(() => {
-    (async () => {
-      // setLoading(true)
-      const response = await fetch(`https://api.rawg.io/api/games/${id}?key=e930d30eb04b4962b85aa272f2925b23`)
-      const serverResponse = await response.json()
-      console.log('GAAAMMEEEE', serverResponse);
-      setGame(serverResponse)
-      // setLoading(false)
-    })()
-  }, [])
+  // useEffect(() => {
+  //   (async () => {
+  //     // setLoading(true)
+  //     const response = await fetch(`https://api.rawg.io/api/games/${id}?key=e930d30eb04b4962b85aa272f2925b23`)
+  //     const serverResponse = await response.json()
+  //     console.log('GAAAMMEEEE', serverResponse);
+  //     setGame(serverResponse)
+  //     // setLoading(false)
+  //   })()
+  // }, [])
 
 
-  useEffect(() => {
-    (async () => {
-      // setLoading(true)
-      const response = await fetch(`https://api.rawg.io/api/games/${id}/movies?key=e930d30eb04b4962b85aa272f2925b23`)
-      const serverResponse = await response.json()
-      console.log('trailer', serverResponse);
-      setTrailer(serverResponse)
-      // setLoading(false)
-    })()
-  }, [game])
+  // useEffect(() => {
+  //   (async () => {
+  //     // setLoading(true)
+  //     const response = await fetch(`https://api.rawg.io/api/games/${id}/movies?key=e930d30eb04b4962b85aa272f2925b23`)
+  //     const serverResponse = await response.json()
+  //     console.log('trailer', serverResponse);
+  //     setTrailer(serverResponse)
+  //     // setLoading(false)
+  //   })()
+  // }, [game])
 
   const [open, setOpen] = React.useState(false);
 
@@ -47,7 +47,7 @@ const GameFromMenu = () => {
         <div className='content-game'>
           <div style={{ display: 'flex', justifyContent: 'space-between' }}>
             <div style={{ margin: '0 25px' }}>
-              <h1>{game.name} <span><img width='26px' src='https://e7.pngegg.com/pngimages/641/241/png-clipart-gold-sticker-star-star-angle-triangle.png' alt="" /></span></h1>
+              <h1>{game.name}</h1>
               <p>Платформы:{game.platforms?.map(el => <span> /{el.platform.name}</span>)}</p>
               <p>Дата релиза: {game.released}</p>
               <p>Жанр: {game.genres?.map(el => <span> /{el.name}</span>)}</p>
@@ -59,14 +59,14 @@ const GameFromMenu = () => {
               <p></p>
               <p>описание :{game.description_raw}</p>
 
-              {
+              {/* {
                 trailer.results?.length
                   ?
                   trailer.results?.map(el => <> <button type="button" onClick={handleOpen}>trailer</button>  <Modaly trailer={el} open={open} handleClose={handleClose} /> </>)
                   // <a href={el.data.max}><button type="button" onClick={handleOpen}>trailer</button></a>
                   :
                   ''
-              }
+              } */}
             </div>
             <div></div>
           </div>
